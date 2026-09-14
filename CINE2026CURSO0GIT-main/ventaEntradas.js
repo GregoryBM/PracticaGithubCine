75c5087b631ef3a98c14dcc8756411453e6b864b
Ghodsian Pérez Farbod <161723679+Borja029@users.noreply.github.com>
2026-09-14 18:32:02 +0200

    addGladidiatorJS
    contenedor de gladiator en js

--- CINE2026CURSO0GIT-main/ventaEntradas.js
+++ CINE2026CURSO0GIT-main/ventaEntradas.js
@@ -1,11 +1,13 @@
-var aPelis=new Array("Que bello es vivir","Solo en casa","Cuento de Navidad");
-var aLoc= new Array(50,120,60);
-var aSalas=new Array("1","2","3");
+var aPelis=new Array("Que bello es vivir","Solo en casa","Cuento de Navidad","Gladiator ||");
+var aLoc= new Array(50,120,60,100);
+var aSalas=new Array("1","2","3","4");
+
 //Almacenamos la localidades vendidas por sala: aVendidas1, aVendidas2 y aVendidas3
 //y las vendidas en la pelicula seleccionada en el array vendidas
 var aVendidas1= new Array();    
 var aVendidas2= new Array();
 var aVendidas3= new Array();
+var aVendidas4= new Array();
 var vendidas=new Array();   
 var asientos=new Array();  //los asientos vendidos en cada compra
 var numButacas=0;   //butacas disponibles en la sala seleccionada
@@ -31,6 +33,9 @@ function seleccionarPeli(){
             break;
         case "3":
             vendidas=aVendidas3.slice(0,aVendidas3.length);
+            break;
+             case "4":
+            vendidas=aVendidas3.slice(0,aVendidas4.length);
             break;
     }
     pintarButacas();    
@@ -94,6 +99,9 @@ function confirmarVenta(){
                 aVendidas2=vendidas.slice(0,vendidas.length);
                 break;
             case "3":
+                aVendidas3=vendidas.slice(0,vendidas.length);
+                break;
+            case "4":
                 aVendidas3=vendidas.slice(0,vendidas.length);
                 break;
             }
